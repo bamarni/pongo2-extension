33f1a82fe976bde6a47a0d31836f1f5351e3b399
Bilal Amarni <bilal.amarni@gmail.com>
2016-11-13 11:54:49 +0100

    add phone filter

--- filters.go
+++ filters.go
@@ -2,12 +2,36 @@ package pongo2extension
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/flosch/pongo2"
 )
 
+var (
+	DefaultLocale = "fr_FR"
+
+	phoneRgx = map[string]*regexp.Regexp{
+		"fr_FR": regexp.MustCompile("^\\+33([1-9])([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})$"),
+	}
+
+	phoneFmt = map[string]string{
+		"fr_FR": "0$1 $2 $3 $4 $5",
+	}
+)
+
 func init() {
+	pongo2.RegisterFilter("phone", filterPhone)
 	pongo2.RegisterFilter("price", filterPrice)
+}
+
+func filterPhone(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	// TODO: allow to override the default locale with a filter param
+	val := phoneRgx[DefaultLocale].ReplaceAllString(
+		in.String(),
+		phoneFmt[DefaultLocale],
+	)
+
+	return pongo2.AsValue(val), nil
 }
 
 func filterPrice(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
